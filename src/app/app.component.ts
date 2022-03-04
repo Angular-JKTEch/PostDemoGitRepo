bc422095148ae5bfadc893b6d178563fb2e86ce0
ngactive99 <ngactive99@gmail.com>
2022-03-04 15:50:19 +0530

    added error handler and added new fields

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,21 +5,25 @@ import UserService from './UserService';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  constructor(private userService:UserService) {}
+  constructor(private userService: UserService) {}
 
   title = 'postdemo';
-  user:User = new User();
+  user: User = new User();
 
-  save(){
+  save() {
     // console.log(this.user.firstname);
     const promise = this.userService.save(this.user);
-    promise.subscribe(function(response) {
-      console.log(response);
-      
-    })
-    
+    promise.subscribe(
+      function (response) {
+        console.log(response); // sucess handler 100-399 which our sucess status codes
+      },
+      function (error) {
+        // error handler 400-599
+        alert(error.message);
+      }
+    );
   }
 }
